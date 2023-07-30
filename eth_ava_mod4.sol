3331ce8a2c4acd4a34e7be11e9240643aab2bda0
sivasankarreddyvattigunta <138890207+sivasankarreddyvattigunta@users.noreply.github.com>
2023-07-30 09:22:32 +0530

    Update eth_ava_mod4.sol

--- eth_ava_mod4.sol
+++ eth_ava_mod4.sol
@@ -2,27 +2,29 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-//0x90F79bf6EB2c4f870365E785982E1f101E93b906 for reference only
+
 contract DegenToken is ERC20 {
 
     constructor() ERC20("Degen", "DGN") {}
-
+     //0x90F79bf6EB2c4f870365E785982E1f101E93b906 for reference
     // Mint new tokens 
     function mint(address to, uint amount) public {
         _mint(to, amount);
     }
-
+    //owner address:0x330c75B827643Bd2D2b0A61e3fD39f80222Ae05B
     // to Transfer tokens 
     function transfer(address to, uint amount) public virtual override returns (bool) {
         _transfer(_msgSender(), to, amount);
         return true;
     }
-
+     event Redeemed(address indexed account, uint amount);
     // to Redeem tokens
-    function redeem(uint amount) public returns (bool) {
+    function redeem(uint amount) public returns (string memory) {
+        require(amount > 0, "Amount to redeem must be greater than 0");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         _burn(msg.sender, amount);
-        return true;
+        emit Redeemed(msg.sender, amount);
+        return "Redeem successful!";
     }
 
     // to Check token balance of an account
@@ -32,8 +34,11 @@ contract DegenToken is ERC20 {
 
     // to Burn tokens from sender's account
     function burn(uint amount) public {
+        require(amount > 0, "Amount to burn must be greater than 0");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         _burn(msg.sender, amount);
     }
+
+    
 }
 
